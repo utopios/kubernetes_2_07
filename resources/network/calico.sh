738f76f7b04c0ae7cecc5c5f35772845ffa66876
ihab abadi <ihab@utopios.net>
2025-07-03 16:38:08 +0200

    update calcio.sh

--- resources/network/calico.sh
+++ resources/network/calico.sh
@@ -1,3 +1,7 @@
+k3d cluster create mon-cluster \
+  --k3s-arg "--flannel-backend=none@server:*" \
+  --k3s-arg "--disable-network-policy@server:*"\
+  --port "30080:30080@loadbalancer"
 # Installer l'opérateur Calico
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 
